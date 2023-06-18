bb6e4e6d52c1317d6b9af18dc12d63413dee70be
ThezZzo <and.belitskiy@gmail.com>
2023-06-19 01:53:30 +0700

    Delete Order

---
+++ Application/Order/Commands/Delete/DeleteOrderCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Order.Commands.Delete;
+
+public class DeleteOrderCommand : IRequest<Unit>
+{
+    public int OrderId { get; set; }
+}
---
+++ Application/Order/Commands/Delete/DeleteOrderCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Exceptions;
+using Application.Order.Commands.Create;
+using Application.Product.Commands.Delete;
+using Infrastructure.Persistance;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Order.Commands.Delete;
+
+public class DeleteOrderCommandHandler : IRequestHandler<DeleteProductCommand, Unit>
+{
+    private readonly ApplicationDbContext _dbContext;
+    
+    public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var order = await _dbContext.Orders.FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+        if (order == null)
+        {
+            throw new NotFoundException(nameof(order), request.Id);
+        }
+        _dbContext.Orders.Remove(order);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
