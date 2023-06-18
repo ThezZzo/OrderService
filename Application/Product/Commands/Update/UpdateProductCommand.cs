7ae511d9053caad14c5dcd056c2b61787c349cb2
ThezZzo <and.belitskiy@gmail.com>
2023-06-18 12:00:10 +0700

    UpdateProduct

---
+++ Application/Product/Commands/Update/UpdateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Product.Commands.Update;
+
+public class UpdateProductCommand : IRequest
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Application/Product/Commands/Update/UpdateProductCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Exceptions;
+using Infrastructure.Persistance;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Product.Commands.Update;
+
+public class UpdateProductCommandHandler
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UpdateProductCommandHandler(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<Unit> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = _dbContext.Products.FirstOrDefaultAsync(p => p.Id == request.Id);
+        if (product == null || product.Id != request.Id)
+        {
+            throw new NotFoundException(nameof(product), request.Id);
+        }
+
+        product.Result.Name = request.Name;
+        product.Result.Price = request.Price;
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
