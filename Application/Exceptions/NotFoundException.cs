9d50cf37a93e28aa9b33deb176bf28817cca013e
ThezZzo <and.belitskiy@gmail.com>
2023-06-18 11:59:54 +0700

    NotFoundException

---
+++ Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string name, object key) : base($"Сущность {name} с ключом {key} не найдена")
+    {
+        
+    }
+}
