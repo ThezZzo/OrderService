94bf700bef5386d220f0eeead2840982d3eabcc5
ThezZzo <and.belitskiy@gmail.com>
2023-06-19 01:50:21 +0700

    Create Order

---
+++ Application/Order/Commands/Create/CreateOrderCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Product.Queries.GetProduct;
+using MediatR;
+
+namespace Application.Order.Commands.Create;
+
+public class CreateOrderCommand : IRequest<Unit>
+{
+    public int ProductId { get; set; }
+    public int Count { get; set; }
+}
---
+++ Application/Order/Commands/Create/CreateOrderCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Product.Commands.Create;
+using Infrastructure.Persistance;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Order.Commands.Create;
+
+public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Unit>
+{
+    private readonly ApplicationDbContext _dbContext;
+    
+    public async Task<Unit> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = new Domain.Entities.Order
+        {
+            Count = request.Count,
+            ProductId = request.ProductId
+        };
+        await _dbContext.Orders.AddAsync(order, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
